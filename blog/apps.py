1c34e7c0c17bbcef4c89745b5f1b4f4c7a54b587
eva-17 <voce@Eva.git>
2023-04-10 14:12:43 -0300

    My Django Girls app, first commit

---
+++ blog/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class BlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'blog'
+#token github|: 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
